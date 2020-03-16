f30f21d14018f7b92d95f1d08900e0ae28dcd0f3
Ashwintlp <62239258+Ashwintlp@users.noreply.github.com>
2020-03-16 17:09:39 +0530

    Update 6.php

--- 6.php
+++ 6.php
@@ -1,9 +1,9 @@
 <?php
 
 
-    $connection = pg_connect( "host=ec2-3-91-112-166.compute-1.amazonaws.com port=5432 dbname=dfao2a1rbfvq49 user=wncysqgsdbushb password=b8188545ffab5ba8643606c50da6cb46c5a5db1ab48dea7dd187e6f66158b70d"
+   
+    $connection = pg_connect( "host=ec2-23-22-156-110.compute-1.amazonaws.com port=5432 dbname=di7aeib8u6nea user=fsyucftmxyxvap password=f70cb55c9e989f35c4b931575b5f28b248be49091d6f143fe3ae3d48bc3f093f"
 );
-
     if(!$connection)
     {
         die("Connection failed: ".pg_last_error());
